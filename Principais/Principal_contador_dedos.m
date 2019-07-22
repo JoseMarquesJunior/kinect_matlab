b616a4df649cd49cce85d62b887331a60210727e
JoseMarquesJunior <31868000+JoseMarquesJunior@users.noreply.github.com>
2019-07-21 21:13:12 -0300

    Rename Principal_contador_dedos.m to Principais/Principal_contador_dedos.m

--- Principais/Principal_contador_dedos.m
+++ Principais/Principal_contador_dedos.m
@@ -1,5 +1,5 @@
 % Classificando imagens diretamente do kinect
-% Esse script carrega os dados de imagens salvos anteriormente; inicia a captura do sensor Kinect; e classifica quantos dedos são mostrados.
+% Esse script carrega os dados de imagens salvos anteriormente; inicia a captura do sensor Kinect; e classifica quantos dedos sÃ£o mostrados.
 
 clear all;
 close all;
@@ -8,13 +8,13 @@ clc;
 % Carregando dados para treinamento do modelo
 load('dados_treinamento_dedos.mat');
 
-% Criação dos modelos para classificação das imagens;
-% ver função "multisvm_adaptado"
+% CriaÃ§Ã£o dos modelos para classificaÃ§Ã£o das imagens;
+% ver funÃ§Ã£o "multisvm_adaptado"
 modelo = svm_multiclasse(matriz_treinamento_dedos,rotulos_treinamento_dedos);
 [precisao_treinamento, matriz_confusao_treinamento] = calcular_precisao_dedos(modelo,matriz_treinamento_dedos,rotulos_treinamento_dedos);
 [precisao_teste, matriz_confusao_teste] = calcular_precisao_dedos(modelo,matriz_teste_dedos,rotulos_teste_dedos);
 
-% Inicialização do sensor kinect; ver função "iniciar_kinect"
+% InicializaÃ§Ã£o do sensor kinect; ver funÃ§Ã£o "iniciar_kinect"
 [himg, colorVid, depthVid] = iniciar_kinect;
 
 % Loop 'while' para cada imagem obtida pelo Kinect
@@ -25,13 +25,13 @@ while(ishandle(himg))
     [colorMap,~,colorMetaData] = getdata(colorVid);
     imshow(colorMap, [0 4096]);
     
-    % Verifica se é encontrado algum esqueleto
+    % Verifica se Ã© encontrado algum esqueleto
     if sum(depthMetaData.IsSkeletonTracked) > 0
         idx = find(depthMetaData.IsSkeletonTracked);
-        % Podem ser encontrados até seis pessoas
-        % idx é o índice referente a pessoa encontrada
+        % Podem ser encontrados atÃ© seis pessoas
+        % idx Ã© o Ã­ndice referente a pessoa encontrada
         if(idx~=0)
-            % Obtendo região de interesse a partir da imagem fornecida pelo
+            % Obtendo regiÃ£o de interesse a partir da imagem fornecida pelo
             % kinect
             limites = gerar_roi(depthMetaData,idx);
            
