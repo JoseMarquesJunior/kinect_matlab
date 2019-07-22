8aae645b94b37413f45e9c4fb6c2d2a9bb11db5f
JoseMarquesJunior <31868000+JoseMarquesJunior@users.noreply.github.com>
2019-07-21 21:13:59 -0300

    Rename Principal_gerar_imagens.m to Principais/Principal_gerar_imagens.m

--- Principais/Principal_gerar_imagens.m
+++ Principais/Principal_gerar_imagens.m
@@ -1,15 +1,15 @@
 %% Gerando imagens para treinamento
-% Esse script salva imagens que serão utilizadas posteriormente para
+% Esse script salva imagens que serÃ£o utilizadas posteriormente para
 % treinamento do classificador de imagens.
 % 
-% Primeiramente, obtém-se o ROI (região de interesse) utilizando o ponto da
-% mão fornecido pelo Kinect.
+% Primeiramente, obtÃ©m-se o ROI (regiÃ£o de interesse) utilizando o ponto da
+% mÃ£o fornecido pelo Kinect.
 
 clear all;
 close all;
 clc;
 
-% Número de imagens para cada gesto
+% NÃºmero de imagens para cada gesto
 qte_imagens = 200;
 
 % Iniciando o Kinect
@@ -22,24 +22,24 @@ cont = 0;   % Contador de imagens
 while(ishandle(himg))
     [depthMap,depthMetaData] = mostrar_video_profundidade(depthVid);
     
-    % Verifica se é encontrado algum esqueleto
+    % Verifica se Ã© encontrado algum esqueleto
     if sum(depthMetaData.IsSkeletonTracked) > 0
         idx = find(depthMetaData.IsSkeletonTracked);
-        % Podem ser encontrados até seis pessoas
-        % idx é o índice referente a pessoa encontrada
+        % Podem ser encontrados atÃ© seis pessoas
+        % idx Ã© o Ã­ndice referente a pessoa encontrada
         
         if(idx~=0)
-            % Obtendo região de interesse a partir da imagem fornecida pelo
+            % Obtendo regiÃ£o de interesse a partir da imagem fornecida pelo
             % kinect
             limites = gerar_roi(depthMetaData,idx);
            
             % Tratando a imagem
             imagem = tratar_imagem(depthMap,limites);
             
-            % Salva a imagem tratada no diretório
+            % Salva a imagem tratada no diretÃ³rio
             salvar_imagem(imagem,depthMetaData);
             
-            % Interrompe o loop quando são salvas o número de imagens
+            % Interrompe o loop quando sÃ£o salvas o nÃºmero de imagens
             % desejadas
             cont = cont + 1;
             if(cont>=qte_imagens)
