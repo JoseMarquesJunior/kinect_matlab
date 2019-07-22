b3182607446e3fb925598b768b633af509220435
JoseMarquesJunior <31868000+JoseMarquesJunior@users.noreply.github.com>
2019-07-21 21:13:36 -0300

    Rename Principal_contador_dedos_ruido.m to Principais/Principal_contador_dedos_ruido.m

--- Principais/Principal_contador_dedos_ruido.m
+++ Principais/Principal_contador_dedos_ruido.m
@@ -1,11 +1,11 @@
-%% Testando dados do contador de dedos com ruÌdo
+%% Testando dados do contador de dedos com ru√≠do
 % Utilizar o MATLAB 2013
 
-% … necess·rio configurar o incremento de ruÌdo e quantidade amostras por
-% ruÌdo nas linhas 28 e 29.
+% √â necess√°rio configurar o incremento de ru√≠do e quantidade amostras por
+% ru√≠do nas linhas 28 e 29.
 
-% As mesmas configuraÁıes de 'porcentagem_ruÌdo' e 'num_amostras' devem ser
-% as mesmas utilizadas na geraÁ„o dos dados com ruÌdo.
+% As mesmas configura√ß√µes de 'porcentagem_ru√≠do' e 'num_amostras' devem ser
+% as mesmas utilizadas na gera√ß√£o dos dados com ru√≠do.
 
 clear all;
 close all;
@@ -19,12 +19,12 @@ matriz_teste_dedos = single(matriz_teste_dedos);
 rotulos_treinamento_dedos = single(rotulos_treinamento_dedos);
 rotulos_teste_dedos = single(rotulos_teste_dedos);
 
-% Configurando porcentagem de ruÌdo desejada (deve ser configurado da mesma
-% forma que na geraÁ„o dos dados com ruÌdos)
+% Configurando porcentagem de ru√≠do desejada (deve ser configurado da mesma
+% forma que na gera√ß√£o dos dados com ru√≠dos)
 porcentagem_ruido = 0:0.02:1;
 num_amostras = 10;
 
-% CriaÁ„o dos modelos para classificaÁ„o das imagens;
+% Cria√ß√£o dos modelos para classifica√ß√£o das imagens;
 for j = 1:length(porcentagem_ruido)
     tic
     j
@@ -39,13 +39,13 @@ end
 media_precisao_treinamento = mean(precisao_treinamento);
 media_precisao_teste = mean(precisao_teste);
 
-% Plotando mÈdia das precisıes obtidas para cada quantidade de ruÌdo
+% Plotando m√©dia das precis√µes obtidas para cada quantidade de ru√≠do
 figure();
 p = plot(porcentagem_ruido,media_precisao_treinamento,porcentagem_ruido,media_precisao_teste);
 axis([0 inf 0 100]);
 legend('Treinamento','Teste');
-xlabel('RuÌdo');
- ylabel('Precis„o [%]');
+xlabel('Ru√≠do');
+ ylabel('Precis√£o [%]');
 set(p(1),'LineWidth',4,'Color','b');
 set(p(2),'LineWidth',4,'Color','g');
 grid on;
